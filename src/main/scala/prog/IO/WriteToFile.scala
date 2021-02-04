f90aeaccab6f641940bc1c76e6f970058cfb33b2
Eragon35 <antipin35@yahoo.com>
2021-02-04 23:39:26 +0300

    Make skeleton to write data to file

--- src/main/scala/prog/IO/WriteToFile.scala
+++ src/main/scala/prog/IO/WriteToFile.scala
@@ -1,5 +1,19 @@
 package prog.IO
 
+import prog.Model.Flat
+
+import java.io.{File, PrintWriter}
+import scala.collection.mutable
+
 class WriteToFile {
 
+  def writeToFile(fileName : String, collection: mutable.ArrayDeque[Flat]) : Unit = {
+    val fileObject = new File(fileName) // Creating a file
+    val printWriter = new PrintWriter(fileObject) // Passing reference of file to the printwriter
+    printWriter.write("<file>") // Writing to the file
+    collection.foreach(x => printWriter.write(x.toXml))
+    printWriter.write("</file>")
+    printWriter.close()
+    println("Data was written to " + fileName)
+  }
 }
