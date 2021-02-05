6c80e82f8b571ea85aff76d7ab560f34d1b3703a
= <=>
2021-02-05 11:46:37 +0300

    Take away warnings & write method that parsed xml to Flat object

--- src/main/scala/prog/Model/Flat.scala
+++ src/main/scala/prog/Model/Flat.scala
@@ -5,10 +5,11 @@ import prog.Model.Transport.Transport
 import prog.Model.View.View
 
 import java.util.{Calendar, Date}
+import scala.xml.{Elem, Node}
 
-class Flat(id: Int = IdGenerator.getId, name: String, coordinates: Coordinates, area: Float, numberOfRooms : Int, furnish: Furnish = Option[Furnish],
-           view: View = Option[View], transport: Transport = Option[Transport], house: House) {
-  val creationDate: Date = Calendar.getInstance().getTime // Fri Jan 08 01:39:17 MSK 2021
+class Flat(id: Int = IdGenerator.getId, name: String, coordinates: Coordinates, creationDate: Date = Calendar.getInstance().getTime ,area: Float,
+           numberOfRooms : Int, furnish: Furnish = Option[Furnish], view: View = Option[View], transport: Transport = Option[Transport], house: House) {
+   //TODO: rework creationDate from "Fri Jan 08 01:39:17 MSK 2021" to "20.08.1997"
   if (id <= 0) throw new IllegalArgumentException("id must be more than 0")
   if (name == null) throw new IllegalArgumentException("name can't be null")
   if (name.isEmpty) throw new IllegalArgumentException("name must be longer than 0")
@@ -22,17 +23,8 @@ class Flat(id: Int = IdGenerator.getId, name: String, coordinates: Coordinates, 
 
 //  TODO: finish toString
   override def toString: String = {
-    val checkNulls = checkingNulls()
-    "Flat - " +
-    "id: " + id +
-    ", name: " + name +
-    ", " + coordinates.toString +
-    ", creation date: " + creationDate +
-    ", area: " + area +
-    ", number of rooms: " + numberOfRooms +
-    ", furnish: " + furnish +
-    checkNulls +
-    ", " + house.toString
+    s"Flat - id: $id, name: $name, $coordinates, creation date: $creationDate, area: $area, " +
+    s"number of rooms: $numberOfRooms, furnish: $furnish, $checkingNulls(), $house"
   }
 
   /**
@@ -46,13 +38,17 @@ class Flat(id: Int = IdGenerator.getId, name: String, coordinates: Coordinates, 
     result
   }
 
-  def toXml () = {
+  /**
+   *
+   * @return xml element that would be written to file
+   */
+  def toXml: Elem = {
     <flat>
       <id>{ id }</id>
       <name>{ name }</name>
       <coordinates>
-        <x>{ coordinates.x }</x>
-        <y>{ coordinates.y }</y>
+        <x>{ coordinates.x() }</x>
+        <y>{ coordinates.y() }</y>
       </coordinates>
       <creation-date>{ creationDate }</creation-date>
       <area>null</area>  <!-- can be null-->
@@ -61,10 +57,27 @@ class Flat(id: Int = IdGenerator.getId, name: String, coordinates: Coordinates, 
       <view>{ view }</view>
       <transport>null</transport>  <!-- can be null-->
       <house>
-        <name>{ house.name }</name>
-        <year>{ house.year }</year>
-        <number-of-lifts>{ house.numberOfFloors }</number-of-lifts>
+        <name>{ house.name() }</name>
+        <year>{ house.year() }</year>
+        <number-of-lifts>{ house.numberOfFloors() }</number-of-lifts>
       </house>
     </flat>
+  }
+
+//  TODO: add validation for input
+  def fromXml(node : Node) : Flat = {
+    val id = (node \ "id").text.toInt
+    val name = (node \ "name").text
+    val coordinates = new Coordinates((node \ "coordinates" \ "x").text.toLong, (node \ "coordinates" \ "y").text.toFloat)
+    val creationDate = new java.text.SimpleDateFormat("dd.MM.yyyy").parse((node \ "creation-date").text)
+    val area = (node \ "area").text.toFloat
+    val numberOfRooms = (node \ "number-of-rooms").text.toInt
+    val furnish = Furnish.withName((node \ "furnish").text)
+    val view = View.withName((node \ "view").text)
+    val transport = Transport.withName((node \ "transport").text)
+    val house = new House((node \ "house" \ "name").text, (node \ "house" \ "year").text.toInt, (node \ "house" \ "number-of-lifts").text.toInt)
+
+
+    new Flat(id, name, coordinates, creationDate, area, numberOfRooms, furnish, view, transport, house)
   }
 }
--- src/main/scala/prog/Model/IdGenerator.scala
+++ src/main/scala/prog/Model/IdGenerator.scala
@@ -2,9 +2,8 @@ package prog.Model
 
 object IdGenerator {
   private var id = 0
-  def serId(id: Int): Unit ={
-    this.id = id
-  }
+
+  def setId (id : Int): Unit = { this.id = id }
   def getId: Int = {
     id = id +1
     id
