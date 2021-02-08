9d3b53605bf53c8b9550984148a212d8a3a765e0
Eragon35 <antipin35@yahoo.com>
2021-02-09 00:23:32 +0300

    Make writer from file

--- src/main/scala/prog/IO/ReadFromFile.scala
+++ src/main/scala/prog/IO/ReadFromFile.scala
@@ -1,11 +1,23 @@
 package prog.IO
 
+import prog.Main.collection
+import prog.Model.{FlatReader, IdGenerator}
+
+import java.io.{BufferedInputStream, File, FileInputStream}
 import scala.xml
+import scala.xml.NodeSeq.Empty.text
+import scala.xml.XML
 
 object ReadFromFile {
   def readXml(fileName : String) = {
-
-    //  TODO set Id in IdGenerator
+//    val inputStream = new BufferedInputStream(new FileInputStream(fileName)
+    val xml = XML.loadFile(new File(fileName))
+    var maxId = -1
+    for (flat <- xml \\ "file" \\ "flat") {
+      collection.addOne(FlatReader.fromXml(flat))
+      if ((flat \ "id").text.toInt > maxId) maxId = (flat \ "id").text.toInt
+    }
+    IdGenerator.setId(maxId)
   }
 
   def readCommands (fileName: String) = {}
--- src/main/scala/prog/IO/WriteToFile.scala
+++ src/main/scala/prog/IO/WriteToFile.scala
@@ -9,8 +9,8 @@ object WriteToFile {
 
   def writeToFile(fileName : String, collection: mutable.ArrayDeque[Flat]) : Unit = {
     val streamWriter = new OutputStreamWriter(new FileOutputStream(fileName))
-    streamWriter.write("<file>\n") // Writing to the file
-    collection.foreach(x => streamWriter.write(x.toXml.toString()))
+    streamWriter.write("<file>\n")
+    collection.foreach(x => streamWriter.write(x.toXml.toString())) // Writing to the file each element of collection
     streamWriter.write("\n</file>")
     streamWriter.close()
     println("Data was written to " + fileName)
