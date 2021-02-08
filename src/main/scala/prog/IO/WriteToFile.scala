42a5c1cac16f841258f1e05970186c7f5831e2e4
Eragon35 <antipin35@yahoo.com>
2021-02-08 23:08:26 +0300

    Rework writer to OutputStreamWriter, it looks like finished

--- src/main/scala/prog/IO/WriteToFile.scala
+++ src/main/scala/prog/IO/WriteToFile.scala
@@ -2,17 +2,17 @@ package prog.IO
 
 import prog.Model.Flat
 
-import java.io.{File, PrintWriter}
+import java.io.{FileOutputStream, OutputStreamWriter}
 import scala.collection.mutable
 
 object WriteToFile {
 
   def writeToFile(fileName : String, collection: mutable.ArrayDeque[Flat]) : Unit = {
-    val printWriter = new PrintWriter(new File(fileName)) // Passing reference to file to the printWriter
-    printWriter.write("<file>\n") // Writing to the file
-    collection.foreach(x => printWriter.write(x.toXml.toString()))
-    printWriter.write("\n</file>")
-    printWriter.close()
+    val streamWriter = new OutputStreamWriter(new FileOutputStream(fileName))
+    streamWriter.write("<file>\n") // Writing to the file
+    collection.foreach(x => streamWriter.write(x.toXml.toString()))
+    streamWriter.write("\n</file>")
+    streamWriter.close()
     println("Data was written to " + fileName)
   }
 
